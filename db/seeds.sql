2ad088cb4f1c71ae53c859a3a1909f14663c37a3
TP <tparks1989@yahoo.com>
2019-04-27 22:26:29 -0400

    one

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Smash Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Mondo Burger');
+INSERT INTO burgers
+    (burger_name, devoured
+    )
+VALUES
+    ('Five Guys Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured
+    )
+VALUES
+    ('Good Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured
+    )
+VALUES
+    ('Shake Shack Burger', true);
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Red Robin Burger');
