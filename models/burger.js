9962fd11a560e9718f29c46e783fa0c7630958e5
TP <tparks1989@yahoo.com>
2019-04-29 23:12:44 -0400

    herokutwo

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,7 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var burger = {
+var cat = {
     all: function (cb) {
         orm.all("burgers", function (res) {
             cb(res);
@@ -17,8 +17,13 @@ var burger = {
         orm.update("burgers", objColVals, condition, function (res) {
             cb(res);
         });
+    },
+    delete: function (condition, cb) {
+        orm.delete("burgers", condition, function (res) {
+            cb(res);
+        });
     }
 };
 
 // Export the database functions for the controller (catsController.js).
-module.exports = burger;
+module.exports = cat;
